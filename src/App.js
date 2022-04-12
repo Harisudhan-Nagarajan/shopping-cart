03dbb998ecf2246d8d86f1a54f3dbd6c758b0c52
Harisudhan-Nagarajan <MY_NAME@example.com>
2022-04-12 16:51:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+} from "@mui/material";
+import { createContext, useEffect, useState } from "react";
+import "./App.css";
+// import { API } from "./global";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { PhoneList } from "./PhoneList";
+import { Cart } from "./Cart";
 
-function App() {
+export const cartCtx = createContext();
+export const API = "http://localhost:9000";
+
+export const currencyFormatter = (number) =>
+  new Intl.NumberFormat("en-IN", { style: "currency", currency: "INR" }).format(
+    number
+  );
+
+export default function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/cart`)
+      .then((data) => data.json())
+      .then((cartItems) => setCart(cartItems));
+  }, []);
+
+  const updateCart = ({ mobile, action }) => {
+    fetch(`${API}/cart?type=${action}`, {
+      method: "PUT",
+      body: JSON.stringify(mobile),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((data) => data.json())
+      .then((latestCart) => setCart(latestCart));
+  };
+
+  const totalCartQty = cart
+    .map((item) => item.qty)
+    .reduce((sum, item) => sum + item, 0);
+
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <cartCtx.Provider value={[cart, updateCart]}>
+        <Box sx={{ flexGrow: 1, marginBottom: "5rem" }}>
+          <AppBar position="fixed">
+            <Toolbar>
+              <Button
+                onClick={() => navigate("/")}
+                color="inherit"
+                sx={{ marginLeft: "auto" }}
+              >
+                Home
+              </Button>
+              <Button color="inherit">Log in</Button>
+              <IconButton color="inherit" onClick={() => navigate("/cart")}>
+                <Badge badgeContent={totalCartQty} color="secondary">
+                  <ShoppingCartIcon />
+                </Badge>
+              </IconButton>
+            
+            </Toolbar>
+          </AppBar>
+        </Box>
+
+        <Routes>
+          <Route path="/" element={<PhoneList />} />
+          <Route path="/cart" element={<Cart setCart={setCart} />} />
+        </Routes>
+        {/* <PhoneList />
+        <Cart /> */}
+      </cartCtx.Provider>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/PhoneList.js
@@ -0,0 +1,37 @@
+import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { API, cartCtx, currencyFormatter } from "./App";
+
+export function PhoneList() {
+  const [mobiles, setMobiles] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/mobiles`)
+      .then((data) => data.json())
+      .then((mbs) => setMobiles(mbs));
+  }, []);
+  return (
+    <div className="phone-list-container">
+      {mobiles.map((mobile) => (
+        <Phone key={mobile._id} mobile={mobile} />
+      ))}
+    </div>
+  );
+}
+function Phone({ mobile }) {
+  const [cart, updateCart] = useContext(cartCtx);
+  return (
+    <div className="phone-container">
+      <img src={mobile.img} alt={mobile.model} className="phone-picture" />
+      <h2 className="phone-name">{mobile.model}</h2>
+      <p className="phone-company">{mobile.company}</p>
+      <p className="phone-price">{currencyFormatter(mobile.price)}</p>
+      <Button
+        variant="contained"
+        onClick={() => updateCart({ mobile, action: "increment" })}
+      >
+        Add to cart
+      </Button>
+    </div>
+  );
+}
